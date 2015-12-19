4f6710eb80b1919f362fdbb2062ff9f880cbc934
Ben Polson <benpolson.zena@gmail.com>
2015-12-19 17:13:37 -0600

    Let's do this

---
+++ src/controller/Controller.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import view.SnakeGUI;
+/**
+ *
+ * @author CT-Master Electricia
+ */
+public class Controller {
+    private SnakeGUI view;
+    
+    public Controller(SnakeGUI v) {
+        this.view = v;
+    }
+}
